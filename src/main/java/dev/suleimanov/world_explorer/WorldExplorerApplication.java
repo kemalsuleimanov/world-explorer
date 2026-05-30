a7b765f8c3342ff86c46a96a419937cf134ce690
kemalsuleimanov <sulkemref@gmail.com>
2026-05-30 15:37:43 -0500

    first commit

---
+++ src/main/java/dev/suleimanov/world_explorer/WorldExplorerApplication.java
@@ -0,0 +1,13 @@
+package dev.suleimanov.world_explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/suleimanov/world_explorer/WorldExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.suleimanov.world_explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
